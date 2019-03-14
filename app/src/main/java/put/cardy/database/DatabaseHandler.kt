a54eb0156da62ff7071fc7727c2425ec7cafa877
sucuncu <sucuncu@consdata.pl>
2019-03-14 14:18:05 +0100

    database handler

--- app/src/main/java/put/cardy/database/DatabaseHandler.kt
+++ app/src/main/java/put/cardy/database/DatabaseHandler.kt
@@ -2,8 +2,10 @@ package put.cardy.database
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteConstraintException
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import put.cardy.model.Card
 
@@ -39,24 +41,89 @@ class DatabaseHandler(context: Context) :
 
 
     @Throws(SQLiteConstraintException::class)
-    fun deleteUser(userid: String): Boolean {
-        // Gets the data repository in write mode
+    fun deleteCard(cardId: Int): Boolean {
         val db = writableDatabase
-        // Define 'where' part of query.
-        val selection = DBContract.UserEntry.COLUMN_USER_ID + " LIKE ?"
-        // Specify arguments in placeholder order.
-        val selectionArgs = arrayOf(userid)
-        // Issue SQL statement.
-        db.delete(DBContract.UserEntry.TABLE_NAME, selection, selectionArgs)
+        val selection = DBContract.CardEntry.CARD_ID + " LIKE ?"
+        val selectionArgs = arrayOf(cardId.toString())
+        db.delete(DBContract.CardEntry.TABLE_NAME, selection, selectionArgs)
 
         return true
     }
 
+    fun updateCard(card: Card): Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(DBContract.CardEntry.NUMBER, card.number)
+        values.put(DBContract.CardEntry.BANKNAME, card.bankName)
+        val _success = db.update(
+            DBContract.CardEntry.TABLE_NAME,
+            values,
+            DBContract.CardEntry.CARD_ID + "=?",
+            arrayOf(card.id.toString())
+        ).toLong()
+        db.close()
+        return Integer.parseInt("$_success") != -1
+    }
+
+
+    fun readCard(cardId: Int): Card? {
+        var card: Card? = null
+        val db = writableDatabase
+        var cursor: Cursor? = null
+        try {
+            cursor = db.rawQuery(
+                "select * from " + DBContract.CardEntry.TABLE_NAME + " WHERE " + DBContract.CardEntry.CARD_ID + "='" + cardId + "'",
+                null
+            )
+        } catch (e: SQLiteException) {
+            db.execSQL(SQL_CREATE_ENTRIES)
+            return null
+        }
+
+        val number: String
+        val bankName: String
+        if (cursor!!.moveToFirst()) {
+            number = cursor.getString(cursor.getColumnIndex(DBContract.CardEntry.NUMBER))
+            bankName = cursor.getString(cursor.getColumnIndex(DBContract.CardEntry.BANKNAME))
+
+            card = Card(cardId, number, bankName)
+            cursor.close()
+
+        }
+        return card
+    }
+
+    fun readAllCards(): ArrayList<Card> {
+        val users = ArrayList<Card>()
+        val db = writableDatabase
+        var cursor: Cursor? = null
+        try {
+            cursor = db.rawQuery("select * from " + DBContract.CardEntry.TABLE_NAME, null)
+        } catch (e: SQLiteException) {
+            db.execSQL(SQL_CREATE_ENTRIES)
+            return ArrayList()
+        }
+
+        var cardId: Int
+        var number: String
+        var bankName: String
+        if (cursor!!.moveToFirst()) {
+            while (cursor.isAfterLast == false) {
+                cardId = cursor.getInt(cursor.getColumnIndex(DBContract.CardEntry.CARD_ID))
+                number = cursor.getString(cursor.getColumnIndex(DBContract.CardEntry.NUMBER))
+                bankName = cursor.getString(cursor.getColumnIndex(DBContract.CardEntry.BANKNAME))
+
+                users.add(Card(cardId, number, bankName))
+                cursor.moveToNext()
+            }
+        }
+        return users
+    }
+
 
     companion object {
-        // If you change the database schema, you must increment the database version.
         val DATABASE_VERSION = 1
-        val DATABASE_NAME = "FeedReader.db"
+        val DATABASE_NAME = "Card.db"
 
         private val SQL_CREATE_ENTRIES =
             "CREATE TABLE " + DBContract.CardEntry.TABLE_NAME + " (" +
--- app/src/main/java/put/cardy/model/Card.kt
+++ app/src/main/java/put/cardy/model/Card.kt
@@ -1,8 +1,5 @@
 package put.cardy.model
 
-class Card {
-    var id: Int = 0
-    var number: String = ""
-    var bankName: String = ""
+class Card(var id: Int, var number: String, var bankName: String) {
 
 }
