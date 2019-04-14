f74f59744362e031f50001870a2a7b7dfed4c83f
sucuncu <sucuncu@consdata.pl>
2019-04-14 10:23:13 +0200

    manage add transaction

--- app/src/main/java/put/cardy/card/CardInfoActivity.kt
+++ app/src/main/java/put/cardy/card/CardInfoActivity.kt
@@ -1,17 +1,24 @@
 package put.cardy.card
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.InputType
-import android.widget.Spinner
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.*
 import kotlinx.android.synthetic.main.card_controllers.*
 import kotlinx.android.synthetic.main.card_info.*
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import put.cardy.R
 import put.cardy.database.CardRepository
 import put.cardy.database.GoalRepository
 import put.cardy.model.Card
 import put.cardy.model.Goal
-import put.cardy.model.GoalType
+
+
 
 
 class CardInfoActivity : AppCompatActivity() {
@@ -27,9 +34,44 @@ class CardInfoActivity : AppCompatActivity() {
         getCardInfo()
 
         fab.setOnClickListener {
-
+            openPopup()
         }
 
+    }
+
+    private fun openPopup() {
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val view = inflater.inflate(R.layout.popup, null)
+
+        val popupWindow = PopupWindow(
+            view,
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        popupWindow.elevation = 10.0F
+
+        val date = view.findViewById<EditText>(R.id.date)
+        date.inputType = InputType.TYPE_NULL
+        date.setText(getActualDate())
+
+        val addButton = view.findViewById<Button>(R.id.button_popup)
+
+        TransitionManager.beginDelayedTransition(root_layout)
+        popupWindow.showAtLocation(
+            root_layout,
+            Gravity.BOTTOM,
+            0,
+            0
+        )
+
+    }
+
+    private fun getActualDate(): String? {
+        val now = DateTime.now()
+        val fmt = DateTimeFormat.forPattern("d MMMM, yyyy")
+        val str = now.toString(fmt)
+        return str
     }
 
     private fun disableInputs() {
